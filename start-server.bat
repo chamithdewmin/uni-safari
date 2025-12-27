b7d71c3ad692f9b1eced47127b4094e8fbc5a940
chamithdewmin <chamithsamarakon@gmail.com>
2025-12-27 15:47:07 +0530

    remove backend files

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting PHP Development Server...
-echo.
-echo Your website will be available at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-php -S localhost:8000
-
